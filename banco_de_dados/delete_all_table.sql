73ebeee05465dcc2d22e195405cac3e8ef3d5038
Lucas Jordão <lucas.jordao.m@gmail.com>
2017-10-08 14:56:14 -0300

    Script exclusão de todas as tabelas existentes

---
+++ banco_de_dados/delete_all_table.sql
@@ -0,0 +1,19 @@
+DROP TABLE SC_Aluno;
+
+DROP TABLE SC_Endereco;
+
+DROP TABLE SC_Equipe;
+
+DROP TABLE SC_Evento:
+
+DROP TABLE SC_Faculdade;
+
+DROP TABLE SC_Fiscal;
+
+DROP TABLE SC_Foguete;
+
+DROP TABLE SC_Lancamento; 
+
+DROP TABLE SC_Turma;
+
+DROP TABLE SC_Lancamento; 
